d798bfdbd75de183f169cc0e502bb34efa210d47
TwickE <118013263+TwickE@users.noreply.github.com>
2025-03-10 16:24:08 +0000

    Started working on the Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-
+import Image from "next/image"
 
 export default function Home() {
     return (
-        <h1 className="text-3xl text-red-500 font-bold underline">
-            Home Page
-        </h1>
+        <main className="max-w-[1320px] min-h-[calc(100vh-196px)] mx-auto flex my-12 bg-">
+            <div className="flex flex-col justify-center gap-5 w-1/2 text-white">
+                <h2 className="text-4xl font-bold">Hi, I am Fred</h2>
+                <h1 className="text-5xl font-bold text-gradient-dark">A Full Stack Developer +<br/>UX Designer</h1>
+                <p>I’m a technology enthusiast with a focus on Web Development. I consider myself a curious individual, always eager to learn new things.</p>
+            </div>
+            <div className=" flex items-center justify-center w-1/2">
+                <div className="bg-dark-photo-bg rounded-[38px] w-fit rotate-5 hover:rotate-0 transition-transform duration-300">
+                    <Image
+                        src="/assets/profilePhoto.webp"
+                        alt="Profile Photo"
+                        width={475}
+                        height={510}
+                        className="border-2 border-secondary rounded-[38px] hover:border-primary transition-border duration-300"
+                    />
+                </div>
+            </div>
+        </main>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <header className='flex gap-5 text-xl underline font-bold p-5'>
+    <header className='flex items-center gap-5 text-xl underline font-bold p-5 w-full h-25 text-white'>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/projects">Projects</Link>
