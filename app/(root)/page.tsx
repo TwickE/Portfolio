f538694fd26ac3fdf7b6d8410906e1656b854435
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2026-01-20 17:01:19 +0000

    Updated the HeroSection text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
         <main className="responsive-container min-h-[calc(100vh-196px)] flex my-12 hero-glow1  max-2xl:flex-col max-2xl:items-center">
             <div className="animate-fade-in-left flex flex-col justify-center gap-2 w-1/2 max-2xl:w-[700px] max-2xl:items-center max-xl:w-full max-lg:px-3">
                 <h2 className="text-4xl font-bold max-5xl:text-[2rem] max-4xl:text-[1.8rem] max-3xl:text-[1.4rem] max-2xl:m-auto max-2xl:text-[2rem] max-xl:text-[1.8rem] max-lg:text-2xl max-md:text-xl max-sm:text-base">Hi, I am Fred</h2>
-                <h1 className="text-5xl font-bold text-gradient mb-2 leading-[1.1] max-5xl:text-[2.7rem] max-4xl:text-4xl max-3xl:text-[2rem] max-2xl:m-auto max-2xl:text-center max-2xl:text-5xl max-xl:text-[2.5rem] max-lg:text-[2rem] max-md:[1.7rem] max-sm:text-xl">A Full Stack Developer + UX Designer</h1>
+                <h1 className="text-5xl font-bold text-gradient mb-2 leading-[1.1] max-5xl:text-[2.7rem] max-4xl:text-4xl max-3xl:text-[2rem] max-2xl:m-auto max-2xl:text-center max-2xl:text-5xl max-xl:text-[2.5rem] max-lg:text-[2rem] max-md:[1.7rem] max-sm:text-xl">A Full Stack and Mobile Developer + UX Designer</h1>
                 <div className="hero-glow2 hidden max-2xl:block">
                     <Image
                         src="/profilePhoto.jpg"
@@ -58,7 +58,7 @@ const HeroSection = () => {
                         className="border-2 border-my-secondary bg-my-photo-background rounded-[38px] hover:border-my-primary transition-colors duration-300 max-2xl:my-8 max-lg:w-full max-lg:h-auto max-md:my-4 max-sm:my-2"
                     />
                 </div>
-                <p className="mb-16 max-4xl:text-[1.1rem] max-3xl:text-base max-3xl:mb-5 max-2xl:text-2xl max-2xl:text-center max-lg:text-[1.1rem] max-lg:w-full max-md:text-base max-sm:text-[0.8rem]">I&apos;m a technology enthusiast with a focus on Web Development. I consider myself a curious individual, always eager to learn new things.</p>
+                <p className="mb-16 max-4xl:text-[1.1rem] max-3xl:text-base max-3xl:mb-5 max-2xl:text-2xl max-2xl:text-center max-lg:text-[1.1rem] max-lg:w-full max-md:text-base max-sm:text-[0.8rem]">I&apos;m a technology enthusiast with a focus on Web and Mobile Development. I consider myself a curious individual, always eager to learn new things.</p>
                 <div className="flex gap-5 items-center max-md:flex-wrap max-md:justify-center">
                     <OutlineButton
                         text="Download CV"
